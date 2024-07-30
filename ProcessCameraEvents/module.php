4ec14bb432bb9d7bf257d1e03638c957501a82df
fisart <1234fisart@gmail.com>
2024-07-30 09:35:23 +0200

    Update module.php

--- ProcessCameraEvents/module.php
+++ ProcessCameraEvents/module.php
@@ -33,7 +33,6 @@ class ProcessCameraEvents extends IPSModule {
             $hooks = json_decode(IPS_GetProperty($ids[0], 'Hooks'), true);
             $found = false;
             foreach ($hooks as $index => $hook) {
-                IPS_LogMessage("HIKAF", "Hook : ".$hook['Hook']."  Webhook ".$find_Hook);
                 if ($hook['Hook'] == $find_Hook) {
                     if ($hook['TargetID'] == $this->InstanceID) {
                         return;
