bb16726a698fe9bf17e8815c5d6205ff513ca0a0
fisart <1234fisart@gmail.com>
2024-08-04 18:53:53 +0200

    Update module.php

--- ProcessCameraEvents/module.php
+++ ProcessCameraEvents/module.php
@@ -51,8 +51,7 @@ class ProcessCameraEvents extends IPSModule {
 
     public function  ProcessHookData() {
         
-        if (IPS_SemaphoreEnter($kameraId."process",1000)) 
-        {
+   
             
             $eggTimerModuleId = $this->ReadPropertyString('EggTimerModuleId');
             if (!IPS_GetModule($eggTimerModuleId)) {
@@ -74,12 +73,7 @@ class ProcessCameraEvents extends IPSModule {
             } else {
                 IPS_LogMessage("HIKMOD","No Data");
             }
-            IPS_SemaphoreLeave($kameraId)."process";
-        }
-        else
-        {
-
-        }
+ 
     }
 
     private function handleMotionData($motionData) {
@@ -92,62 +86,72 @@ class ProcessCameraEvents extends IPSModule {
         $password= $this->ReadPropertyString('Password');
         $motion_active = $this->ReadPropertyInteger('MotionActive');
         $kameraId = $this->manageVariable($parent, $motionData['channelName'], 0, 'Motion', true, 0, "");
-        SetValueBoolean($kameraId, true);
-        $delay_still_active  = false;
-        $eggTimerId = @IPS_GetObjectIDByName("Egg Timer", $kameraId);
-        if ($eggTimerId) {
-            $activ_id = @IPS_GetObjectIDByName("Aktiv",  $eggTimerId );
-            $delay_still_active = GetValueBoolean($activ_id );
-        }
-        if(!$delay_still_active)
-        { 
-            $kameraName_var_id = $this->manageVariable($kameraId, $motionData['ipAddress'], 3, '~TextBox', true, 0, "");
-
-            SetValueString($kameraName_var_id, $motionData['eventDescription']);
-
-            $username = GetValueString($this->manageVariable($kameraId, "User Name", 3, '~TextBox', true, 0, $username));
-            $password = GetValueString($this->manageVariable($kameraId, "Password", 3, '~TextBox', true, 0, $password ));
-            $dateTime = $this->manageVariable($kameraId, "Date and Time", 3, '~TextBox', true, 0, "");
-            SetValueString($dateTime, $motionData['dateTime']);
-            if ($username != $notSetYet && $password != $notSetYet) {
-                $savePath .= $motionData['ipAddress'] . ".jpg";
-                $this->downloadHikvisionSnapshot($motionData['ipAddress'], $channelId, $username, $password, $savePath);
-                sleep(2);
-                $this->manageMedia($kameraId, "Last_Picture", $savePath);
-            } else {
-                echo "Please set UserName and Password in Variable";
+        if (IPS_SemaphoreEnter($kameraId."process",1000)) 
+        {
+            SetValueBoolean($kameraId, true);
+            $delay_still_active  = false;
+            $eggTimerId = @IPS_GetObjectIDByName("Egg Timer", $kameraId);
+            if ($eggTimerId) {
+                $activ_id = @IPS_GetObjectIDByName("Aktiv",  $eggTimerId );
+                $delay_still_active = GetValueBoolean($activ_id );
             }
-            if (IPS_SemaphoreEnter($kameraId,1000)) 
-            {
-                $eggTimerId = @IPS_GetObjectIDByName("Egg Timer", $kameraId);
-                if ($eggTimerId) {
-                    SetValueInteger(IPS_GetObjectIDByName("Zeit in Sekunden", $eggTimerId), $motion_active);
-                    RequestAction(IPS_GetObjectIDByName("Aktiv", $eggTimerId), true);
+            if(!$delay_still_active)
+            { 
+                $kameraName_var_id = $this->manageVariable($kameraId, $motionData['ipAddress'], 3, '~TextBox', true, 0, "");
+
+                SetValueString($kameraName_var_id, $motionData['eventDescription']);
+
+                $username = GetValueString($this->manageVariable($kameraId, "User Name", 3, '~TextBox', true, 0, $username));
+                $password = GetValueString($this->manageVariable($kameraId, "Password", 3, '~TextBox', true, 0, $password ));
+                $dateTime = $this->manageVariable($kameraId, "Date and Time", 3, '~TextBox', true, 0, "");
+                SetValueString($dateTime, $motionData['dateTime']);
+                if ($username != $notSetYet && $password != $notSetYet) {
+                    $savePath .= $motionData['ipAddress'] . ".jpg";
+                    $this->downloadHikvisionSnapshot($motionData['ipAddress'], $channelId, $username, $password, $savePath);
+                    sleep(2);
+                    $this->manageMedia($kameraId, "Last_Picture", $savePath);
                 } else {
-                    $insId = IPS_CreateInstance($this->ReadPropertyString('EggTimerModuleId'));
-                    IPS_SetName($insId, "Egg Timer");
-                    IPS_SetParent($insId, $kameraId);
-                    IPS_ApplyChanges($insId);
-                    RequestAction(IPS_GetObjectIDByName("Aktiv", $insId), true);
-                    SetValueInteger(IPS_GetObjectIDByName("Zeit in Sekunden", $insId), $motion_active);
-                    $eid = IPS_CreateEvent(0);
-                    IPS_SetEventTrigger($eid, 4, IPS_GetObjectIDByName("Aktiv", $insId));
-                    IPS_SetParent($eid, $kameraId);
-                    IPS_SetEventAction($eid, "{75C67945-BE11-5965-C569-602D43F84269}", ["VALUE" => false]);
-                    IPS_SetEventActive($eid, true);
-                    IPS_SetEventTriggerValue($eid, false);
+                    echo "Please set UserName and Password in Variable";
                 }
-                IPS_SemaphoreLeave($kameraId);
+                if (IPS_SemaphoreEnter($kameraId,1000)) 
+                {
+                    $eggTimerId = @IPS_GetObjectIDByName("Egg Timer", $kameraId);
+                    if ($eggTimerId) {
+                        SetValueInteger(IPS_GetObjectIDByName("Zeit in Sekunden", $eggTimerId), $motion_active);
+                        RequestAction(IPS_GetObjectIDByName("Aktiv", $eggTimerId), true);
+                    } else {
+                        $insId = IPS_CreateInstance($this->ReadPropertyString('EggTimerModuleId'));
+                        IPS_SetName($insId, "Egg Timer");
+                        IPS_SetParent($insId, $kameraId);
+                        IPS_ApplyChanges($insId);
+                        RequestAction(IPS_GetObjectIDByName("Aktiv", $insId), true);
+                        SetValueInteger(IPS_GetObjectIDByName("Zeit in Sekunden", $insId), $motion_active);
+                        $eid = IPS_CreateEvent(0);
+                        IPS_SetEventTrigger($eid, 4, IPS_GetObjectIDByName("Aktiv", $insId));
+                        IPS_SetParent($eid, $kameraId);
+                        IPS_SetEventAction($eid, "{75C67945-BE11-5965-C569-602D43F84269}", ["VALUE" => false]);
+                        IPS_SetEventActive($eid, true);
+                        IPS_SetEventTriggerValue($eid, false);
+                    }
+                    IPS_SemaphoreLeave($kameraId);
+                }
+                else
+                {
+                    IPS_SemaphoreLeave($kameraId)."process";
+                    return false;
+                }
             }
             else
             {
+                IPS_SemaphoreLeave($kameraId)."process";
                 return false;
-            }
+            } 
+            IPS_SemaphoreLeave($kameraId)."process";
         }
         else
         {
-            return false;
-        }   
+
+        }  
     }
 
     private function parseEventNotificationAlert($xmlString) {
