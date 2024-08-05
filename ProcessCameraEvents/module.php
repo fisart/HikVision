80d8c9cad13abce7708da3784e96fabff673c730
fisart <1234fisart@gmail.com>
2024-08-05 15:53:57 +0200

    Update module.php

--- ProcessCameraEvents/module.php
+++ ProcessCameraEvents/module.php
@@ -140,20 +140,20 @@ class ProcessCameraEvents extends IPSModule {
                 }
                 else
                 {
-                    IPS_LogMessage("HIKMOD","Leave process Semphore because kameraID Semaphore is set ".$kameraId);
-                    IPS_SemaphoreLeave($kameraId)."process";
-                    return false;
+                    //IPS_LogMessage("HIKMOD","Leave process Semphore because kameraID Semaphore is set ".$kameraId);
+                    //IPS_SemaphoreLeave($kameraId)."process";
+                    //return false;
                 }
-                IPS_LogMessage("HIKMOD","Leave process Semphore Delay was not active ".$kameraId);
-                IPS_SemaphoreLeave($kameraId)."process";                
+                //IPS_LogMessage("HIKMOD","Leave process Semphore Delay was not active ".$kameraId);
+                //IPS_SemaphoreLeave($kameraId)."process";                
             }
             else
             {
-                IPS_LogMessage("HIKMOD","Leave process Semphore because Delay is still active no Data processing ".$kameraId);
-                IPS_SemaphoreLeave($kameraId)."process";
-                return false;
+                //IPS_LogMessage("HIKMOD","Leave process Semphore because Delay is still active no Data processing ".$kameraId);
+                //IPS_SemaphoreLeave($kameraId)."process";
+                //return false;
             } 
-            IPS_LogMessage("HIKMOD","Leave process Semphore because Data has been processed ".$kameraId);
+            IPS_LogMessage("HIKMOD","Leave process Semaphore  ".$kameraId);
             IPS_SemaphoreLeave($kameraId)."process";
         }
         else
